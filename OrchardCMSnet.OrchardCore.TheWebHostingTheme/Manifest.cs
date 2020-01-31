0a62ef6f38cc65c606faa2757336728e07f96bd3
orchardcmsnet <blazor@orchardcms.net>
2020-01-30 22:43:01 -0700

    Added Razor

---
+++ OrchardCMSnet.OrchardCore.TheWebHostingTheme/Manifest.cs
@@ -0,0 +1,19 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "TheWebHostingTheme",
+    Author = "OrchardCMS.net",
+    Website = "http://OrchardCMS.net",
+    Version = "2.0.0",
+    Description = "A SaaS Multi-Tenant Web Hosting Theme."
+)]
+
+//[assembly: OrchardCore.DisplayManagement.Manifest.Theme(
+//    Name = "The default ModernBusiness theme",
+//    BaseTheme = "TheTheme",
+//    Author = "The Orchard Team",
+//    Website = "https://orchardproject.net",
+//    Version = "0.0.1",
+//    Description = "Theme is created by Start Bootstrap"
+//)]
+
